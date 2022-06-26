7509a853d7641ad167479b46b76b366ff3ad82a4
user <user@WIN-U74PASC9CHT>
2022-06-26 13:52:56 +0300

    Добавьте файлы проекта.

---
+++ Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ServicesApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.resultsCommands = new System.Windows.Forms.RichTextBox();
+            this.inputServices = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // resultsCommands
+            // 
+            this.resultsCommands.Location = new System.Drawing.Point(819, 12);
+            this.resultsCommands.Name = "resultsCommands";
+            this.resultsCommands.Size = new System.Drawing.Size(432, 426);
+            this.resultsCommands.TabIndex = 1;
+            this.resultsCommands.Text = "";
+            // 
+            // inputServices
+            // 
+            this.inputServices.Location = new System.Drawing.Point(548, 12);
+            this.inputServices.Name = "inputServices";
+            this.inputServices.Size = new System.Drawing.Size(210, 426);
+            this.inputServices.TabIndex = 2;
+            this.inputServices.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1312, 450);
+            this.Controls.Add(this.inputServices);
+            this.Controls.Add(this.resultsCommands);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "ServiceApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox resultsCommands;
+        private System.Windows.Forms.RichTextBox inputServices;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Security;
+
+
+namespace ServicesApp
+{
+    public partial class Main : Form
+    {
+
+
+
+        string[] services = { };        
+        public Main()
+        {
+            InitializeComponent();
+            var exePath = AppDomain.CurrentDomain.BaseDirectory;
+            services = File.ReadAllLines("helpingFiles/servicesName.csv");
+
+
+
+        }
+
+        private string correctCommand(string nameApp, string Com)
+        { 
+            Encoding utf = Encoding.GetEncoding("windows-1251");
+            Encoding win = Encoding.GetEncoding("ibm866");
+
+            Process process = new Process();
+            process.StartInfo.FileName = nameApp;
+            process.StartInfo.Arguments = Com;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.UseShellExecute = false;
+           
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+
+            StreamReader reader = process.StandardOutput;
+            string output = reader.ReadToEnd();
+
+            process.WaitForExit();
+            process.Close();
+
+            Console.Read();
+
+            return utf.GetString(Encoding.Convert(win, utf, utf.GetBytes(output)));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[] locationButton = { 20, 20 };
+            int buttonCount = 0;
+            foreach(string service in services)
+            {
+                string getService = correctCommand("cmd", $@"/c sc query {service}");
+                resultsCommands.Text += "\n" + getService + "----------\n";
+                if (!(getService.Contains("Указанная служба не установлена.")))
+                {
+                    Button bt = new Button();
+                    bt.Text =  service.Substring(0, 1).ToUpper() + service.Substring(1);
+                    bt.FlatStyle = FlatStyle.Flat;
+                    bt.FlatAppearance.BorderSize = 2;
+                    bt.Size = new Size(100, 40);
+                    bt.Location = new Point(locationButton[0], locationButton[1]);
+
+                    if (getService.Contains("STOPPED")) bt.BackColor = Color.Red;
+                    else bt.BackColor = Color.Lime;
+
+                    bt.Click += buttonClick;
+
+                    this.Controls.Add(bt);
+
+                    inputServices.Text += service + "\n";
+
+                    locationButton[1] += 60;
+                    buttonCount++;
+                    if (buttonCount % 5 == 0) { locationButton[0] += 140; locationButton[1] = 20; };
+                }
+            }
+            
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            string service = bt.Text;
+            string output;
+            if (bt.BackColor == Color.Lime)
+            {
+                output = correctCommand("cmd", $@"/c net stop {service}");
+                bt.BackColor = Color.Red;
+               
+            }
+            else
+            {
+                output = correctCommand("cmd", $@"/c net start {service}");
+                bt.BackColor = Color.Lime;
+                
+            }
+  
+            resultsCommands.Text += "\n" + output + "----------\n";
+        }
+    }
+}
